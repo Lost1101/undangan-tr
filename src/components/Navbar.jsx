ea56a0714e7d12a8dc52f4e0dabf010c93262f83
Lost1101 <kamilah.insani60@gmail.com>
2024-09-27 15:51:25 +0700

    tes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,13 @@ import { ImGift } from "react-icons/im";
 import { FaArrowsUpDownLeftRight } from "react-icons/fa6";
 import { FaVolumeHigh } from "react-icons/fa6";
 import klajik from "../assets/klajik.jpeg";
-import Button from "./Button";
 
 export default function Navbar(){  
     return(
         <div>
             <div>
-                <Button icon={<FaArrowsUpDownLeftRight className="ml-3"/>} morestyle={'fixed top-5 right-2 bg-white rounded-full px-0 w-10 h-10'}/>
-                <Button icon={<FaVolumeHigh className="ml-3"/>} morestyle={'fixed top-16 right-2 bg-white rounded-full px-0 w-10 h-10'}/>
+                <button className={"fixed top-5 right-2 bg-white rounded-full px-0 w-10 h-10"}><span className='mt-1 mr-2'><FaArrowsUpDownLeftRight className="my-3 mx-3"/></span></button>
+                <button className={"fixed top-20 right-2 bg-white rounded-full px-0 w-10 h-10"}><span className='mt-1 mr-2'><FaVolumeHigh className="my-3 mx-3"/></span></button>
             </div>
             <div className="fixed z-10 bottom-0 left-0 right-0 flex justify-center">
                 <div className="bg-white w-5/6 p-3 rounded-t-xl">
