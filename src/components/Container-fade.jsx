e9255438631f33cc5cd916f01334849f37c342bc
Lost1101 <kamilah.insani60@gmail.com>
2024-11-09 19:16:59 +0700

    revisi gradasi

--- src/components/Container-fade.jsx
+++ src/components/Container-fade.jsx
@@ -3,11 +3,9 @@ import PropTypes from 'prop-types';
 export default function ContainerFade({children}){
     return(
         <div>
-            <div className="bg-gradient-to-t from-undangan-300 h-32 w-full"></div>
             <div className="bg-undangan-300 p-5 py-20">
                 {children}
             </div>
-            <div className="bg-gradient-to-b from-undangan-300 h-32 w-full"></div>
         </div>
     )
 }
